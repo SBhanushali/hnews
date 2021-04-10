8236a4130d9b32cf194be7bf32813e9d380c6f3a
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-04-11 03:27:52 +0530

    Configure react app from scratch and integrate tailwind

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div class="py-8 px-8 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+      <div class="mx-auto text-center space-y-2 sm:text-left">
+        <div class="space-y-0.5">
+          <p class="text-lg text-black font-semibold">React Tailwind</p>
+          <p class="mx-auto text-center text-gray-500 font-medium">Template</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
