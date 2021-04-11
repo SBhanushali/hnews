e994555a3b12265a6de9dcf34ab96882f84657f9
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-04-11 23:40:23 +0530

    A placeholder footer for future

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="sticky top-28 h-20 hidden lg:block text-sm text-gray-500 w-60">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Sagittis purus sit
+        amet volutpat consequat mauris nunc.
+      </p>
+      <p>Built using HackerNews API</p>
+      <p>
+        Design inspiration <a src="">Nicolas Meuzard</a>{" "}
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
