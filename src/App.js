b6c5c7ac7b2b55d1f4886367d5f4678ab388a240
SBhanushali <shivam.n.bhanushali@gmail.com>
2021-04-11 23:41:03 +0530

    Finalize the layout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
-import { NewspaperIcon, SunIcon } from "@heroicons/react/solid";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
+import NewsFeed from "./components/NewsFeed";
+import SideMenu from "./components/SideMenu";
 
 const App = () => {
   return (
-    <div className="min-h-screen bg-gray-100	">
+    <div className="min-h-screen bg-gray-100">
       <Navbar />
+      <div className="lg:mx-5 lg:ml-8 flex flex-col justify-around lg:flex-row">
+        <SideMenu />
+        <NewsFeed />
+        <Footer />
+      </div>
     </div>
   );
 };
